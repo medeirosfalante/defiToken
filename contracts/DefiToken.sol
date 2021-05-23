b700ff9e2f4a5359d390f3687eaba403218099c4
Rafael Medeiros <rafaelgurudesign@gmail.com>
2021-05-23 20:06:50 -0300

    add basic token

---
+++ contracts/DefiToken.sol
@@ -0,0 +1,208 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract DefiToken is ERC20, AccessControl {
+    using SafeMath for uint256;
+    using Address for address;
+
+    mapping(address => uint256) private _rOwned;
+    mapping(address => uint256) private _tOwned;
+    mapping(address => mapping(address => uint256)) private _allowances;
+
+    mapping(address => bool) private _isExcludedFromFee;
+
+    mapping(address => bool) private _isExcluded;
+    address[] private _excluded;
+
+    uint256 private constant MAX = ~uint256(0);
+    uint256 private _tTotal = 1000000000 * 10**6 * 10**9;
+    uint256 private _rTotal = (MAX - (MAX % _tTotal));
+    uint256 private _tFeeTotal;
+
+    uint8 private _decimals = 9;
+    address private _receiverLiquid;
+
+    uint256 public _taxFee = 5;
+    uint256 private _previousTaxFee = _taxFee;
+
+    uint256 public _liquidityFee = 5;
+    uint256 private _previousLiquidityFee = _liquidityFee;
+
+    bool inSwapAndLiquify;
+    bool public swapAndLiquifyEnabled = true;
+
+    uint256 public _maxTxAmount = 5000000 * 10**6 * 10**9;
+    uint256 private numTokensSellToAddToLiquidity = 500000 * 10**6 * 10**9;
+
+    event MinTokensBeforeSwapUpdated(uint256 minTokensBeforeSwap);
+    event SwapAndLiquifyEnabledUpdated(bool enabled);
+    event SwapAndLiquify(
+        uint256 tokensSwapped,
+        uint256 ethReceived,
+        uint256 tokensIntoLiqudity
+    );
+
+    modifier lockTheSwap {
+        inSwapAndLiquify = true;
+        _;
+        inSwapAndLiquify = false;
+    }
+
+    constructor(
+        address _pancakeRouterAddress,
+        string memory _name,
+        string memory _symbol
+    ) ERC20(_name, _symbol) {
+        _rOwned[_msgSender()] = _rTotal;
+        _receiverLiquid = _msgSender();
+
+        //exclude owner and this contract from fee
+        _isExcludedFromFee[_receiverLiquid] = true;
+        _isExcludedFromFee[address(this)] = true;
+
+        emit Transfer(address(0), _msgSender(), _tTotal);
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return _decimals;
+    }
+
+    function totalSupply() public view override returns (uint256) {
+        return _tTotal;
+    }
+
+    function transfer(address recipient, uint256 amount)
+        public
+        override
+        returns (bool)
+    {
+        _transfer(_msgSender(), recipient, amount);
+        return true;
+    }
+
+    function allowance(address owner, address spender)
+        public
+        view
+        override
+        returns (uint256)
+    {
+        return _allowances[owner][spender];
+    }
+
+    function approve(address spender, uint256 amount)
+        public
+        override
+        returns (bool)
+    {
+        _approve(_msgSender(), spender, amount);
+        return true;
+    }
+
+    function isExcludedFromReward(address account) public view returns (bool) {
+        return _isExcluded[account];
+    }
+
+    function totalFees() public view returns (uint256) {
+        return _tFeeTotal;
+    }
+
+    function includeInReward(address account)
+        external
+        onlyRole(DEFAULT_ADMIN_ROLE)
+    {
+        require(_isExcluded[account], "Account is already excluded");
+        for (uint256 i = 0; i < _excluded.length; i++) {
+            if (_excluded[i] == account) {
+                _excluded[i] = _excluded[_excluded.length - 1];
+                _tOwned[account] = 0;
+                _isExcluded[account] = false;
+                _excluded.pop();
+                break;
+            }
+        }
+    }
+
+    function excludeFromFee(address account)
+        public
+        onlyRole(DEFAULT_ADMIN_ROLE)
+    {
+        _isExcludedFromFee[account] = true;
+    }
+
+    function includeInFee(address account) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        _isExcludedFromFee[account] = false;
+    }
+
+    function setTaxFeePercent(uint256 taxFee)
+        external
+        onlyRole(DEFAULT_ADMIN_ROLE)
+    {
+        _taxFee = taxFee;
+    }
+
+    function setLiquidityFeePercent(uint256 liquidityFee)
+        external
+        onlyRole(DEFAULT_ADMIN_ROLE)
+    {
+        _liquidityFee = liquidityFee;
+    }
+
+    function setMaxTxPercent(uint256 maxTxPercent)
+        external
+        onlyRole(DEFAULT_ADMIN_ROLE)
+    {
+        _maxTxAmount = _tTotal.mul(maxTxPercent).div(10**2);
+    }
+
+    function setSwapAndLiquifyEnabled(bool _enabled)
+        public
+        onlyRole(DEFAULT_ADMIN_ROLE)
+    {
+        swapAndLiquifyEnabled = _enabled;
+        emit SwapAndLiquifyEnabledUpdated(_enabled);
+    }
+
+    //to recieve ETH from uniswapV2Router when swaping
+    receive() external payable {}
+
+    function _reflectFee(uint256 rFee, uint256 tFee) private {
+        _rTotal = _rTotal.sub(rFee);
+        _tFeeTotal = _tFeeTotal.add(tFee);
+    }
+
+    function _transfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual override {
+        require(from != address(0), "ERC20: transfer from the zero address");
+        require(to != address(0), "ERC20: transfer to the zero address");
+        require(amount > 0, "Transfer amount must be greater than zero");
+        if (!hasRole(DEFAULT_ADMIN_ROLE, from))
+            require(
+                amount <= _maxTxAmount,
+                "Transfer amount exceeds the maxTxAmount."
+            );
+
+        //transfer amount, it will take tax, burn, liquidity fee
+        _tokenTransfer(from, to, amount, false);
+    }
+
+    //this method is responsible for taking all fee, if takeFee is true
+    function _tokenTransfer(
+        address sender,
+        address recipient,
+        uint256 amount,
+        bool takeFee
+    ) private {}
+
+    function renounceOwnership() public virtual onlyRole(DEFAULT_ADMIN_ROLE) {
+        revokeRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+}
